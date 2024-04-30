e9bed75a5eb76442e583e2acb6c4c05df88160de
Prabaha <prabaha.gangopadhyay@yale.edu>
2024-04-30 14:39:16 -0400

    edit syntax of function call from scripts

--- classify_gaze_behav.py
+++ classify_gaze_behav.py
@@ -6,25 +6,28 @@ Created on Tue Apr  9 10:25:48 2024
 @author: pg496
 """
 
-from util import *
-from filter_behavior import *
-
 import matplotlib.pyplot as plt
+import numpy as np
+
+import util
+import filter_behavior
+
 
 # Determine root data directory based on whether it's running on a cluster or not
 is_cluster = True
-root_data_dir = get_root_data_dir(is_cluster)
+root_data_dir = util.get_root_data_dir(is_cluster)
 # Get subfolders within the root data directory
-session_paths = get_subfolders(root_data_dir)
+session_paths = util.get_subfolders(root_data_dir)
 # Extract meta-information from session paths
-meta_info_list = extract_meta_info(session_paths)
+meta_info_list = util.extract_meta_info(session_paths)
 # Extract OT and NAL doses from meta-information and convert to numpy array
 otnal_doses = np.array([[meta_info['OT_dose'], meta_info['NAL_dose']] for meta_info in meta_info_list], dtype=np.float64)
 # Find unique doses and their indices
-unique_doses, dose_inds, session_categories = get_unique_doses(otnal_doses)
-labelled_gaze_positions = extract_labelled_gaze_positions(unique_doses, dose_inds, meta_info_list, session_paths, session_categories)
+unique_doses, dose_inds, session_categories = util.get_unique_doses(otnal_doses)
+labelled_gaze_positions = filter_behavior.extract_labelled_gaze_positions(
+    unique_doses, dose_inds, meta_info_list, session_paths, session_categories)
 # Have to write this function for nn training
-saccades, saccade_labels = extract_saccades_with_labels(labelled_gaze_positions)
+saccades, saccade_labels = filter_behavior.extract_saccades_with_labels(labelled_gaze_positions)
 
 saccade_lengths = [saccade.shape[0] for saccade in saccades]
 # Plot the histogram
@@ -35,6 +38,7 @@ plt.title('Histogram of Saccade Lengths')
 plt.grid(True)
 plt.show()
 
+"""
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 
@@ -74,3 +78,4 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 
 # Train the model
 model.fit(padded_saccades, np.array(one_hot_labels), epochs=10, batch_size=32, validation_split=0.2)
+"""
